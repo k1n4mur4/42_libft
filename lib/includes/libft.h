034a5f3808ac9aef9c676afc4dc53ac74f14035e
kinamura <i.kosuke0801@gmail.com>
2024-10-13 16:25:47 +0900

    change

--- lib/includes/libft.h
+++ lib/includes/libft.h
@@ -6,7 +6,7 @@
 /*   By: kinamura <kinamura@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/16 13:45:34 by kinamura          #+#    #+#             */
-/*   Updated: 2024/10/13 16:20:37 by kinamura         ###   ########.fr       */
+/*   Updated: 2024/10/13 16:25:35 by kinamura         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,21 +51,21 @@ void				*ft_memchr(const void *s, int c, size_t n);
 int					ft_memcmp(const void *s1, const void *s2, size_t n);
 char				*ft_strnstr(const char *haystack, const char *needle,
 						size_t len);
-int					ft_hextoi(int c);
-int					ft_atoi(const char *str);
-long				ft_strtol(const char *str, char **endptr, int base);
-void				*ft_calloc(size_t count, size_t size);
 char				*ft_strdup(const char *s1);
 char				*ft_substr(char const *s, unsigned int start, size_t len);
 char				*ft_strjoin(char const *s1, char const *s2);
 char				*ft_strtrim(char const *s1, char const *set);
 char				**ft_split(char const *s, char c);
-char				*ft_itoa(int n);
 char				*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 void				ft_striteri(char *s, void (*f)(unsigned int, char *));
 int					ft_abs(int i);
-int					ft_min(int a, int b);
+int					ft_atoi(const char *str);
+void				*ft_calloc(size_t count, size_t size);
+int					ft_hextoi(int c);
+char				*ft_itoa(int n);
 int					ft_max(int a, int b);
+int					ft_min(int a, int b);
+long				ft_strtol(const char *str, char **endptr, int base);
 void				ft_swap(int *a, int *b);
 void				ft_putchar_fd(char c, int fd);
 void				ft_putstr_fd(char *s, int fd);
