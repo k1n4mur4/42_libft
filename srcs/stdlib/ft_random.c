2ba387905cd5c7940fbbf822acdccc88d3ee6d65
kinamura <i.kosuke0801@gmail.com>
2025-07-20 03:06:39 +0900

    [fix] norm

--- srcs/stdlib/ft_random.c
+++ srcs/stdlib/ft_random.c
@@ -6,7 +6,7 @@
 /*   By: kinamura <kinamura@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/19 19:19:53 by kinamura          #+#    #+#             */
-/*   Updated: 2025/07/20 00:40:43 by kinamura         ###   ########.fr       */
+/*   Updated: 2025/07/20 03:06:24 by kinamura         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,8 @@ long	ft_random(void)
 	int			stack;
 
 	rand += (long)&stack;
+	rand ^= rand << 5;
 	rand ^= rand << 13;
 	rand ^= rand >> 17;
-	rand ^= rand << 5;
 	return (rand);
 }
