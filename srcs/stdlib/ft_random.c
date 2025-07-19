38fa888fdcfa23e3fe3dbe19ac0522df935f1496
kinamura <i.kosuke0801@gmail.com>
2025-07-19 19:20:49 +0900

    [add] ft_random

---
+++ srcs/stdlib/ft_random.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_random.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kinamura <kinamura@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/19 19:19:53 by kinamura          #+#    #+#             */
+/*   Updated: 2025/07/19 19:19:57 by kinamura         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+long	ft_random(void)
+{
+	static long	rand;
+	int			stack;
+
+	rand += (long)&stack;
+	rand ^= rand << 13;
+	rand ^= rand >> 17;
+	rand ^= rand << 5;
+	return (rand);
+}
