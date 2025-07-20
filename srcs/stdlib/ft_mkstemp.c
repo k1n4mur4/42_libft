f4a8ca709fe12f5035458a951c2e04a6fa1cbc48
kinamura <i.kosuke0801@gmail.com>
2025-07-20 14:33:30 +0900

    [fix] ft_mkstemp

--- srcs/stdlib/ft_mkstemp.c
+++ srcs/stdlib/ft_mkstemp.c
@@ -6,7 +6,7 @@
 /*   By: kinamura <kinamura@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/20 02:59:09 by kinamura          #+#    #+#             */
-/*   Updated: 2025/07/20 14:30:22 by kinamura         ###   ########.fr       */
+/*   Updated: 2025/07/20 14:32:48 by kinamura         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,5 @@
 
 int	ft_mkstemp(char *template)
 {
-	int		fd;
-	char	*temp_file;
-
-	temp_file = ft_mktemp(template);
-	fd = open(temp_file, O_RDWR | O_CREAT | O_EXCL, 0600);
-	return (fd);
+	return (open(ft_mktemp(template), O_RDWR | O_CREAT | O_EXCL, 0600));
 }
